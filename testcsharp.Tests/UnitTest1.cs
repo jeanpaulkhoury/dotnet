9118d527da6e07eb95713eedf946af293636e787
jeanpaulkhoury <jeanpaulkhoury1@gmail.com>
2023-11-07 12:42:27 +0200

    changes

--- testcsharp.Tests/UnitTest1.cs
+++ testcsharp.Tests/UnitTest1.cs
@@ -1,10 +1,32 @@
-namespace testcsharp.Tests;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
 
-public class UnitTest1
+namespace testcsharp.Tests
 {
-    [Fact]
-    public void Test1()
+    public class MiddlewareTests : IClassFixture<WebApplicationFactory<testcsharp.Startup>>
     {
+        private readonly WebApplicationFactory<testcsharp.Startup> _factory;
 
+        public MiddlewareTests(WebApplicationFactory<testcsharp.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_EndpointReturnsHelloWorld()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/"); // Assuming your endpoint is the root.
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal("Hello World, from ASP.NET!", responseString);
+        }
     }
 }
